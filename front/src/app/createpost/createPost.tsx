b9b918a9b3f0c9b5db43cd09c0d8a81673ba0b7d
Kevin Hu <hukevin69@gmail.com>
2025-05-07 11:01:28 -0700

    create post skeleton

---
+++ front/src/app/createpost/createPost.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { User } from "./user";
+
+import styles from "./page.module.css";
+
+export default function CreatePost() {
+  
+  return (
+    <div className={styles.main}>
+      <div id={styles.left}>
+        <img src="default-avatar.png" alt="placeholder" style={{height: "100px"}}></img>
+      </div>
+      <div id="page">
+        <h1>Page</h1>
+        <h2>Subtitle</h2>
+        <p>Lorem ipsum dit alor dlor ahwt.</p>
+      </div>
+    </div>
+  );
+}
---
+++ front/src/app/createpost/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useState, useEffect } from "react";
+import { User } from "./user";
+import CreatePost from "./createPost";
+
+import styles from "./page.module.css";
+
+export default function page() {
+  
+  return (
+    <>
+    <CreatePost/>
+    </>
+  );
+}
