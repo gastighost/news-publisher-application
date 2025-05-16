29c9799282849135a93accacd81df9392cd94ebf
gastighost <gacaro51597@gmail.com>
2025-05-16 10:38:47 -0700

    add api route for admin to view posts for approval or rejection

--- backend/src/services/postService.ts
+++ backend/src/services/postService.ts
@@ -30,6 +30,32 @@ export const getAllPosts = async (offset: number, limit: number) => {
   });
 };
 
+export async function getAllPostsAdmin(offset = 0, limit = 5) {
+  return await prisma.post.findMany({
+    skip: offset,
+    take: limit,
+    orderBy: { date: "desc" },
+    select: {
+      id: true,
+      title: true,
+      subtitle: true,
+      titleImage: true,
+      content: true,
+      category: true,
+      date: true,
+      commentsEnabled: true,
+      author: {
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          userStatus: true,
+        },
+      },
+    },
+  });
+}
+
 export const getApprovedPostById = async (postId: number) => {
   const post = await prisma.post.findUnique({
     where: { id: postId, approved: true },
